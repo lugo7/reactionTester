951bc0182c9c878ce68c72fc246af6e10ceef922
Michael Lugo <lugo.michael.77@gmail.com>
2022-04-28 01:21:58 -0400

    Redesign and bug fixes

--- reactionTester.js
+++ reactionTester.js
@@ -1,3 +1,4 @@
+//returns a random hex code color
 function getRandomColor() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
@@ -6,69 +7,66 @@ function getRandomColor() {
   }
   return color;
 }
+
+//50/50 chance of being a circle or square
 function getRandomShape(){
   var circle = (Math.random() <= 0.5) ? 1 : 2;
   if(circle === 2){
     document.getElementById('box').style.borderRadius='50%';
   }
 }
-function randomPosition(){
-  var left = Math.floor(Math.random()*document.getElementById('playArea').clientWidth);
-  var top = Math.floor(Math.random()*document.getElementById('playArea').clientHeight);
-  document.getElementById('box').style.left=left+'px';
-  document.getElementById('box').style.top=top+'px';
+
+//needs to appear only within given element
+function randomPosition(elem){
+  let vPos = Math.floor(Math.random()*((elem.getBoundingClientRect().bottom-100)-elem.getBoundingClientRect().top)+elem.getBoundingClientRect().top);
+  let hPos = Math.floor(Math.random()*((elem.getBoundingClientRect().right-100)-elem.getBoundingClientRect().left)+elem.getBoundingClientRect().left);
+  document.getElementById('box').style.left=hPos+'px';
+  document.getElementById('box').style.top=vPos+'px';
 }
+
 var clickedTime; var createdTime; var reactionTime; var i = 1;
-function appearingBox(){
-  createdTime=Date.now();
+
+//Display given element
+function appearingBox(elem){
   let time = Math.floor(Math.random()*3000);
   setTimeout(function(){
     getRandomShape();
-    randomPosition();
+    randomPosition(elem);
     document.getElementById('box').style.backgroundColor=getRandomColor();
     document.getElementById('box').style.display='block';
   },time);
+  createdTime=Date.now();
 }
 
-var Table = function(){
-  var x = document.getElementById('tableStart');
-  this.x = x;
-  this.i = i;
-  this.currentField = function(){
-    this.x = x;
-    if(this.i>8){
-      this.i=1;
-    }
-    if(this.i>4){
-      this.x = this.x.children[this.i-4];
-      this.x.children[3].style.backgroundColor = 'grey';
-    }
-    else{
-      this.x = this.x.children[this.i];
-      this.x.children[1].style.backgroundColor = 'grey';
-    }
-  }
-  this.tableInsert = function(){
-    if(this.i>4){
-      this.x.children[3].innerHTML = reactionTime+'s';
-      this.x.children[3].style.backgroundColor = '#391568';
-    }
-    else{
-      this.x.children[1].innerHTML = reactionTime+'s';
-      this.x.children[1].style.backgroundColor = '#391568';
-    }
-    this.i=this.i+1;
-  }
+function rankingInput(time){
+  var cell=document.getElementsByTagName('td');
+  cell[counter].textContent=reactionTime+'s';
 }
 
-document.getElementById('box').addEventListener('click',function(){
+function getTime(){
   clickedTime=Date.now();
   reactionTime=(clickedTime-createdTime)/1000;
-  scoreboard.tableInsert();
-  this.style.display='none';
-  scoreboard.currentField();
-  appearingBox();
+  rankingInput(reactionTime);
+  counter+=4;
+  if(counter==21){
+    counter=3;
+  }
+  else if(counter==24){
+    //check win or lose condition
+  }
+}
+
+//var scoreboard = new Table();
+//scoreboard.currentField();
+var counter = 1;
+document.getElementById('startGame').addEventListener('click',function(){
+  document.getElementById('overlay').style.display='none';
+  document.getElementById('startGame').removeEventListener('click',function(){});
+  var field = document.getElementById('playArea');
+  document.getElementById('box').addEventListener('click',function(){
+    this.style.display='none';
+    getTime();
+    appearingBox(field);
+  });
+  appearingBox(field);
 });
-var scoreboard = new Table();
-scoreboard.currentField();
-appearingBox();
